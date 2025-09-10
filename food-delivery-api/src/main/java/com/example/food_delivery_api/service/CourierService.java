026f46fc3d853a79bf86b156e2879eb23c4720e6
Yehezkiel Stephanus Austin <yehezkiel.stephanus@gmail.com>
2025-09-10 10:24:34 +0700

    feat(create-courier)=add create courier

--- food-delivery-api/src/main/java/com/example/food_delivery_api/service/CourierService.java
+++ food-delivery-api/src/main/java/com/example/food_delivery_api/service/CourierService.java
@@ -1,7 +1,31 @@
 package com.example.food_delivery_api.service;
 
+import com.example.food_delivery_api.dto.courier.CreateCourierRequest;
+import com.example.food_delivery_api.dto.courier.CreateCourierResponse;
+import com.example.food_delivery_api.dto.customer.CreateCustomerRequest;
+import com.example.food_delivery_api.dto.customer.CreateCustomerResponse;
+import com.example.food_delivery_api.entity.CustomerEntity;
+import com.example.food_delivery_api.repository.CourierRepository;
+import com.example.food_delivery_api.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class CourierService {
+
+    @Autowired
+    private CourierRepository courierRepository;
+
+    public CreateCourierResponse createCustomer(CreateCourierRequest request){
+        CustomerEntity customer = CustomerEntity.builder()
+                .name(request.getName())
+                .createdAt(LocalDateTime.now())
+                .build();
+        return CreateCourierResponse.builder()
+                .name(customer.getName())
+                .createdAt(customer.getCreatedAt())
+                .build();
+    }
 }

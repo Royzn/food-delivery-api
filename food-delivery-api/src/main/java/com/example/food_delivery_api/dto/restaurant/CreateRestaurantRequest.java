92eeade4aebb8d5cca234f215a111412bd4d90cc
Royzn <royzenanggatama@gmail.com>
2025-09-10 09:58:06 +0700

    feat: add creating restaurant feature

---
+++ food-delivery-api/src/main/java/com/example/food_delivery_api/dto/restaurant/CreateRestaurantRequest.java
@@ -0,0 +1,21 @@
+package com.example.food_delivery_api.dto.restaurant;
+
+import com.example.food_delivery_api.entity.MenuEntity;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CreateRestaurantRequest {
+    @NotEmpty
+    @JsonProperty("restaurant_name")
+    private String restaurantName;
+}
--- food-delivery-api/src/main/java/com/example/food_delivery_api/service/RestaurantService.java
+++ food-delivery-api/src/main/java/com/example/food_delivery_api/service/RestaurantService.java
@@ -1,7 +1,28 @@
 package com.example.food_delivery_api.service;
 
+import com.example.food_delivery_api.dto.restaurant.CreateRestaurantRequest;
+import com.example.food_delivery_api.dto.restaurant.CreateRestaurantResponse;
+import com.example.food_delivery_api.entity.RestaurantEntity;
+import com.example.food_delivery_api.repository.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RestaurantService {
+    @Autowired
+    RestaurantRepository restaurantRepository;
+
+    public ResponseEntity<CreateRestaurantResponse> createRestaurant(CreateRestaurantRequest req){
+        RestaurantEntity restaurant = RestaurantEntity.builder()
+                .name(req.getRestaurantName())
+                .build();
+
+        RestaurantEntity savedRestaurant = restaurantRepository.save(restaurant);
+
+        return ResponseEntity.ok(CreateRestaurantResponse.builder()
+                        .restaurantId(savedRestaurant.getRestaurantId())
+                        .restaurantName(savedRestaurant.getName())
+                .build());
+    }
 }
